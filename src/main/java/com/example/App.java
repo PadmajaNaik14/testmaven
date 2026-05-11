e7ca0fb4897f256dbdd02a985268a6eed1069dc0
PadmajaNaik14 <padmajagnaik@gmail.com>
2026-05-11 09:56:47 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "padmaja g naik 1BI23CS145" );
     }
 }
